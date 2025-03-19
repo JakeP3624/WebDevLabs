20f018001e1ccdb1f216b1701c206ed99ec24edd
JakeP3624 <jakepaje@andrew.cmu.edu>
2025-03-18 22:28:08 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -35,18 +35,18 @@ function getOrdinal(num) {
     return num + (suffixes[(remainder - 20) % 10] || suffixes[remainder] || suffixes[0]);
 }
 
-function findTheBanana(array) {
-    let count = 0;
-    array.forEach((item, index) => {
-        count++;
-        if (item === "Banana") {
-            alert("We found a Banana in the " + getOrdinal(count) + " array");
-        }
-    });
-}
+// function findTheBanana(array) {
+//     let count = 0;
+//     array.forEach((item, index) => {
+//         count++;
+//         if (item === "Banana") {
+//             alert("We found a Banana in the " + getOrdinal(count) + " array");
+//         }
+//     });
+// }
 
-findTheBanana(L1);
-findTheBanana(L2);
+// findTheBanana(L1);
+// findTheBanana(L2);
 
 function greetingFunc() {
     var d = new Date();
@@ -65,3 +65,48 @@ function greetingFunc() {
 }
 
 greetingFunc();
+
+function addYear() {
+    const year = new Date().getFullYear();
+    document.getElementById("copyYear").textContent = `© ${year}`;
+}
+
+function showList() {
+    var list = document.getElementById("myList");
+    var button = document.getElementById("showButton");
+    list.style.display = "block";
+    button.style.display = "none";
+}
+
+$(document).ready(function () {
+    $('#readMoreBtn').click(function () {
+        $('#shortBio').hide();
+        $('#longBio').show();
+        $('#readMoreBtn').hide();
+        $('#readLessBtn').show();
+    });
+
+    $('#readLessBtn').click(function () {
+        $('#longBio').hide();
+        $('#shortBio').show();
+        $('#readLessBtn').hide();
+        $('#readMoreBtn').show();
+    });
+});
+
+function validateForm() {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const comment = document.getElementById("comment");
+    const message = document.getElementById("validationMessage");
+    
+    if (!name.checkValidity()) {
+      message.innerHTML = name.validationMessage;
+    } else if (!email.checkValidity()) {
+      message.innerHTML = email.validationMessage;
+    } else if (!comment.checkValidity()) {
+      message.innerHTML = comment.validationMessage;
+    } else {
+      message.innerHTML = "Form submitted successfully!";
+    }
+}
